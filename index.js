c8591f4c3611eade38141c91de70e7b305bbd971
Ahmed-Alabadla <eng.ahmedalabadla@gmail.com>
2024-09-17 18:39:00 +0300

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const {
   createTask,
   getTasks,
@@ -24,6 +26,7 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //  ============= CONNECTION TO DB =============
 mongoose
